3b6a3ae8bcb3ea2adc529e3213317d27f6f38a7e
9uiLe <kobappleple@gmail.com>
2025-11-15 12:01:33 +0900

    chore: fix support version

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "tino-macro",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v13),
+        .iOS(.v18),
+        .macOS(.v15),
     ],
     products: [
         .library(
